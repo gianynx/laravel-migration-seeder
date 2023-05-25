10cc13567ce314a35dc54945f54b6944b699de51
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-25 08:38:43 +0200

    modify home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <section class="container">
+        <h1 class="text-center fw-bold text-danger">Trenitalia Tickets</h1>
         <div class="row pt-5 pb-5">
             @foreach ($trains as $train)
                 <div class="col-12 col-sm-6 col-md-4 col-lg-4 mt-4 mb-4">
@@ -32,10 +33,10 @@
                                     <span class="text-danger fs-4">Numero carrozze:</span>
                                     <span class="fs-5">{{ $train['number_carriages'] }}</span>
                                 </div>
-                                <div>
+                                {{-- <div>
                                     <span class="text-danger fs-4">Ritardo:</span>
                                     <span class="fs-5">{{ $train['delay'] }}</span> <span class="fs-5">minuti</span>
-                                </div>
+                                </div> --}}
                                 <div>
                                     <span class="text-danger fs-4">Categoria:</span>
                                     <span class="fs-5">{{ $train['train_type'] }}</span>
